3ae472355033ebb2a60fc0552e65ac7bd1e68ed9
danila <kiryanov.danya98@mail.ru>
2023-12-27 00:15:25 +0500

    commit 15-20

---
+++ api/src/Auth/Entity/User/Role.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Auth\Entity\User;
+
+use Webmozart\Assert\Assert;
+
+class Role
+{
+    public const ADMIN = 'admin';
+
+    public const USER = 'user';
+
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        Assert::oneOf($name, [
+            self::USER,
+            self::ADMIN,
+        ]);
+
+        $this->name = $name;
+    }
+
+    public static function user(): self
+    {
+        return new self(self::USER);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
